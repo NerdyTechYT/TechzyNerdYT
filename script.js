ad8bc95139be3682ae6d3d646c7c66e19a268c3e
NerdyTechYT <nerdztechyt@gmail.com>
2025-08-17 11:54:11 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.getElementById('menu-toggle');
-const navLinks = document.getElementById('nav-links');
-
-if (toggleBtn && navLinks) {
-  toggleBtn.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-  });
-}
